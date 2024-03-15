1d85f319f92459f778ce6bdc1f0d259532fbce70
Krishna Thakkar <work.krishnat@gmail.com>
2024-03-15 09:34:03 +0530

    contact page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Header from "./components/Layout/Header";
 import Home from "./components/Home/Home";
+import Footer from "./components/Layout/Footer";
+import Contact from './components/Contact/Contact'
+
 import "./styles/app.scss";
 import "./styles/header.scss";
 import "./styles/home.scss";
 import "./styles/founder.scss";
 import "./styles/menu.scss";
 import "./styles/footer.scss";
-import Footer from "./components/Layout/Footer";
+import "./styles/contact.scss";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -19,6 +22,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </Router>
--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { AiFillInstagram, AiFillYoutube, AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 function Footer() {
-  return <div>Updating footer file </div>;
+  return (
+    <footer>
+      <div>
+        <h2>MBA Burger Wala</h2>
+
+        <p>We are trying to give you the best taste possible.</p>
+        <br />
+
+        <em>We give attention to genuine feedback.</em>
+
+        <strong>All right received @mbaburgerwala</strong>
+      </div>
+
+      <aside>
+        <h4>Follow Us</h4>
+
+        <a href="#">
+          <AiFillLinkedin />
+        </a>
+        <a href="https://instagram.com/krishna_.thakkar">
+          <AiFillInstagram />
+        </a>
+        <a href="https://github.com/krishnathakkar29">
+          <AiFillGithub />
+        </a>
+      </aside>
+    </footer>
+  )
 }
 
 export default Footer;
